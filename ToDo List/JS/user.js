9f40a4415a197865600e6086aa1d0ca6d816018d
Onwuka Chisom Stanley <43853290+StanleyTee@users.noreply.github.com>
2019-07-07 05:53:42 +0000

    Services no longer required

--- ToDo List/JS/user.js
+++
@@ -1,7 +0,0 @@
-if (document.getElementById("username").value == " ") {
-	alert("Enter username");
-}else{
-function storeUserName() {
-	sessionStorage.setItem("username",document.getElementById("username").value);
-	}
-}
